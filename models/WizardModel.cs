9f2d3063aeab64c2e0a849c5f12bed66f4b0ead0
LHT082024 <LHT082024@gmail.com>
2025-02-19 09:44:29 +0100

    making model classes

---
+++ models/WizardModel.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace idk.models
+{
+    public class WizardModel
+    {
+        public string? Name { get; set; }
+        public string? MagicType { get; set; }
+        public int PowerLvl { get; set; }
+
+    }
+}
