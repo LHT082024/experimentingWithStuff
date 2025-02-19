223564baf3dcfb884b90b12ae9638dd8111386c8
LHT082024 <LHT082024@gmail.com>
2025-02-19 11:16:14 +0100

    changed more on Holy magic

--- Allmagic.cs
+++ Allmagic.cs
@@ -10,7 +10,7 @@ namespace idk
         public List<ModelOfMagic> modelOfMagicss = new List<ModelOfMagic>(){
             new ModelOfMagic {Element = "Eldritch", NatureInspo = "Water, Lightning, Squid,", PowerlvlAdd = 10},
             new ModelOfMagic {Element = "Darkness", NatureInspo = "Ice, Shadow, Demon", PowerlvlAdd = 9},
-            new ModelOfMagic {Element = "Holy", NatureInspo = "Air, Angel", PowerlvlAdd = 9},
+            new ModelOfMagic {Element = "Holy", NatureInspo = "Air, Light, Angel", PowerlvlAdd = 9},
             new ModelOfMagic {Element = "Fire", NatureInspo = "Fire,  Dragon", PowerlvlAdd = 5},
             new ModelOfMagic {Element = "Ice", NatureInspo = "Frost, Giants", PowerlvlAdd = 5},
             new ModelOfMagic {Element = "EnchantedWeapon", NatureInspo = "Weapons", PowerlvlAdd = 3}
