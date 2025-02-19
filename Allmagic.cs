c55e0391fac81af8e0780b02eba1a12d8e216f33
LHT082024 <LHT082024@gmail.com>
2025-02-19 11:15:18 +0100

    changed magic types

--- Allmagic.cs
+++ Allmagic.cs
@@ -8,9 +8,9 @@ namespace idk
     public class Allmagic
     {
         public List<ModelOfMagic> modelOfMagicss = new List<ModelOfMagic>(){
-            new ModelOfMagic {Element = "Eldritch", NatureInspo = "Water, lightning, Squid,", PowerlvlAdd = 10},
-            new ModelOfMagic {Element = "Darkness", NatureInspo = "Ice, Shadows", PowerlvlAdd = 9},
-            new ModelOfMagic {Element = "Holy", NatureInspo = "Air, bird", PowerlvlAdd = 9},
+            new ModelOfMagic {Element = "Eldritch", NatureInspo = "Water, Lightning, Squid,", PowerlvlAdd = 10},
+            new ModelOfMagic {Element = "Darkness", NatureInspo = "Ice, Shadow, Demon", PowerlvlAdd = 9},
+            new ModelOfMagic {Element = "Holy", NatureInspo = "Air, Angel", PowerlvlAdd = 9},
             new ModelOfMagic {Element = "Fire", NatureInspo = "Fire,  Dragon", PowerlvlAdd = 5},
             new ModelOfMagic {Element = "Ice", NatureInspo = "Frost, Giants", PowerlvlAdd = 5},
             new ModelOfMagic {Element = "EnchantedWeapon", NatureInspo = "Weapons", PowerlvlAdd = 3}
