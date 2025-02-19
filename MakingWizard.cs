7831d56bd7e53064dcd5a318c87b02072e36c809
LHT082024 <LHT082024@gmail.com>
2025-02-19 11:11:59 +0100

    removed whitespace

--- MakingWizard.cs
+++ MakingWizard.cs
@@ -16,7 +16,6 @@ namespace idk
         {
             wizard.Name = "Luka";
             wizard.magic = allmagic.modelOfMagicss[0];
-
             Console.WriteLine($"your name is: {wizard.Name}\n {wizard.Name}'s magic type is {wizard.magic.Element}\n {wizard.Name} powerlevel {wizard.PowerLvl}");
         }
     }
