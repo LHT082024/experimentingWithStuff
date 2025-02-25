3c5083fe431e7590459efdc481af9655b2a3466a
LHT082024 <LHT082024@gmail.com>
2025-02-25 12:01:34 +0100

    trying to replace lists with  interfaces

--- Testingstuff.cs
+++ Testingstuff.cs
@@ -7,48 +7,11 @@ using idk.models;
 
 namespace idk
 {
-    public class Testingstuff : ICharacter
+    public class Testingstuff :
     {
-        public string? Name { get; set; }
-        public ModelOfSpecies? species { get; set; }
-        public ModelOfMagic? magic { get; set; }
-        public int PowerLvl { get; set; }
-        public int Mana { get; set; }
-        public int Health { get; set; }
 
         public void newCharacter()
         {
-
-            bool restart = false;
-
-            while (!restart)
-            {
-                bool nameIsRight = false;
-                while (!nameIsRight)
-                {
-                    Console.WriteLine("\nwhat is your name? (name cannot contain more then 9 characters)");
-                    string? Name = Console.ReadLine();
-
-                    if (string.IsNullOrWhiteSpace(Name))
-                    {
-                        Console.WriteLine("Name cannot be blank");
-                        Console.ReadKey();
-                    }
-                    else if (Name.Length >= 10)
-                    {
-                        Console.WriteLine("name is to long");
-                        Console.ReadKey();
-
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Your name is: {Name}");
-                        nameIsRight = true;
-                        Console.ReadKey();
-                    }
-                }
-
-            }
 
         }
     }
